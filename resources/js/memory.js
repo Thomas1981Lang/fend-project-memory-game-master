f8e5b8e0e9da2c16ac422426f54ba1568886f77b
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-03-16 18:16:01 +0100

    add Shuffle Function

--- resources/js/memory.js
+++ resources/js/memory.js
@@ -1,24 +1,25 @@
-var voc = [
+var vocArray = [
     ['dog', 'Hund'],
-    ['cat', 'Katze'] //,
-    //['snake', 'Schlange'],
-    //['lion', 'Löwe'],
-    //['mouse', 'Maus'],
-    //['bug', 'Käfer'],
-    //['ant', 'Ameise'],
-    //['horse', 'Pferd'],
-    //['cow', 'Kuh'],
-    //['sheep', 'Schaf']
+    ['cat', 'Katze'],
+    ['snake', 'Schlange'],
+    ['lion', 'Löwe'],
+    ['mouse', 'Maus'],
+    ['bug', 'Käfer'],
+    ['ant', 'Ameise'],
+    ['horse', 'Pferd'],
+    ['cow', 'Kuh'],
+    ['sheep', 'Schaf']
 ];
 
 
 
 window.onload = function () {
 
+    game();
+
+    restart();
 
 
 
 
-
-    
 } //onload
--- resources/js/memory.lib.js
+++ resources/js/memory.lib.js
@@ -2,13 +2,24 @@ var welcome = function () {
     location.reload();
 }
 
+var game = function () {
+    document.getElementsByTagName('button')[0].onclick = function () {
+        document.getElementById('game').className = 'game showElement';
+        document.getElementById('info').className = 'game hiddenElement';
+    }
+}
+
 var win = function () {
+    document.getElementById('info').className = 'game showElement';
+    document.getElementById('game').className = 'game hiddenElement';
     document.getElementsByTagName('h1')[0].innerHTML = 'Congratulations';
     document.getElementsByTagName('button')[0].className = 'hiddenElement';
     formular();
 }
 
 var lost = function () {
+    document.getElementById('info').className = 'game showElement';
+    document.getElementById('game').className = 'game hiddenElement';
     document.getElementsByTagName('h1')[0].innerHTML = 'Game Over';
     var h2 = document.createElement('h2')
     h2.innerHTML = 'I\'m sorry you lost';
@@ -18,7 +29,7 @@ var lost = function () {
 
 var formular = function () {
     document.getElementsByTagName('form')[0].className = 'showElement';
-    document.getElementById('winner').submit = function (event) {
+    document.getElementById('winner').onsubmit = function (event) {
         event.preventDefault();
         document.getElementsByTagName('form').className = 'hiddenElement';
         document.getElementsByTagName('button')[0].className = 'button showElement';
@@ -27,10 +38,37 @@ var formular = function () {
 
 // TODO: reset
 var reset = function () {
+    //TODO:
+}
 
+var restart = function () {
+    document.getElementsByClassName('restart')[0].onclick = function () {
+        welcome();
+    }
+}
+var singleArray = function (multiArray) {
+    var i, singleArray = [];
+
+    for (i = 0; i < multiArray.length; i++) {
+        singleArray = singleArray.concat(multiArray[i]);
+    }
+
+    return singleArray;
 }
 
 
+var shuffle = function (singleArray) {
+    var currentIndex = singleArray.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = singleArray[currentIndex];
+        singleArray[currentIndex] = singleArray[randomIndex];
+        singleArray[randomIndex] = temporaryValue;
+    }
+    return singleArray;
+}
 
 
 
