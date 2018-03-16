bce5a414d69c77b8b92ca958614b8ad4ebb22740
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-03-16 10:19:44 +0100

    optimize gamesection

---
+++ resources/js/memory.js
@@ -0,0 +1,24 @@
+var voc = [
+    ['dog', 'Hund'],
+    ['cat', 'Katze'] //,
+    //['snake', 'Schlange'],
+    //['lion', 'Löwe'],
+    //['mouse', 'Maus'],
+    //['bug', 'Käfer'],
+    //['ant', 'Ameise'],
+    //['horse', 'Pferd'],
+    //['cow', 'Kuh'],
+    //['sheep', 'Schaf']
+];
+
+
+
+window.onload = function () {
+
+
+
+
+
+
+    
+} //onload
